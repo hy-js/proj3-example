2ff80ed72c80f18453750d4e835bafd7649ec8b9
RHYS <62794614+hy-js@users.noreply.github.com>
2022-03-01 11:54:31 +1100

    fix node-server

--- app.js
+++ app.js
@@ -1,33 +1,14 @@
-// Imports express module
-const express = require('express')
-const data = require('./Data.js')
+const http = require('http');
 
-// Initialise express server
-const app = express()
-const PORT = 3000 || process.env.PORT
+// Create an instance of the http server to handle HTTP requests
+let app = http.createServer((req, res) => {
+  // Set a response type of plain text for the response
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-// Routes
-app.get('/', (req, res) => {
-   res.send('Welcome to our schedule website')
-})
+  // Send back a response and end the connection
+  res.end('Hello World!\n');
+});
 
-app.get('/goodbye', (req, res) => {
-   res.send('<h1>cya</h1>')
-})
-
-app.get('/ping', (req, res) => {
-   res.send('pong')
-})
-
-// JSON
-app.get('/api/users', (req, res) => {
-   res.json(data.users)
-})
-
-app.get('/api/posts', (req, res) => {
-   res.json(data.posts)
-})
-
-
-app.listen(PORT);
-console.log('Express server running on port 3000');
+// Start the server on port 3000
+app.listen(3000, '127.0.0.1');
+console.log('Node server running on port 3000');
